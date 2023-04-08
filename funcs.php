66ab1a285cc58684ac0e243c5817e551fdc8605f
Ivan Katsarenko <111686070+KacIvan@users.noreply.github.com>
2023-04-08 18:13:48 +0300

    Add all files

---
+++ funcs.php
@@ -0,0 +1,98 @@
+<?php
+
+function debug($data)
+{
+    echo '<pre>' . print_r($data, 1) . '</pre>';
+}
+
+function registration(): bool
+{
+    global $pdo;
+
+    $login = !empty($_POST['login']) ? trim($_POST['login']) : '';
+    $pass = !empty($_POST['pass']) ? trim($_POST['pass']) : '';
+
+    if (empty($login) || empty($pass)) {
+        $_SESSION['errors'] = 'Поля логин/пароль не могут быть пустыми!';
+        return false;
+    }
+
+    $res = $pdo->prepare("SELECT COUNT(*) FROM users WHERE login = ?");
+    $res->execute([$login]);
+    if ($res->fetchColumn()) {
+        $_SESSION['errors'] = 'Данное имя уже используется!';
+        return false;
+    }
+
+    $pass = password_hash($pass, PASSWORD_DEFAULT);
+    $res = $pdo->prepare("INSERT INTO users (login, pass) VALUES (?, ?)");
+    if ($res->execute([$login, $pass])) {
+        $_SESSION['success'] = 'Успешная регистрация :)';
+        return true;
+    } else {
+        $_SESSION['errors'] = 'Ошибка регистрации :(';
+        return false;
+    }
+}
+
+function login(): bool
+{
+    global $pdo;
+
+    $login = !empty($_POST['login']) ? trim($_POST['login']) : '';
+    $pass = !empty($_POST['pass']) ? trim($_POST['pass']) : '';
+
+    if (empty($login) || empty($pass)) {
+        $_SESSION['errors'] = 'Поля логин/пароль не могут быть пустыми!';
+        return false;
+    }
+
+    $res = $pdo->prepare("SELECT * FROM users WHERE login = ?");
+    $res->execute([$login]);
+    if (!$user = $res->fetch()) {
+        $_SESSION['errors'] = 'Неверный логин или пароль!';
+        return false;
+    }
+
+    if (!password_verify($pass, $user['pass'])) {
+        $_SESSION['errors'] = 'Неверный логин или пароль!';
+        return false;
+    } else {
+        $_SESSION['success'] = 'Успешная авторизация!';
+        $_SESSION['user']['name'] = $user['login'];
+        $_SESSION['user']['id'] = $user['id'];
+        return true;
+    }
+}
+
+function save_message(): bool
+{
+    global $pdo;
+    $message = !empty($_POST['message']) ? trim($_POST['message']) : '';
+
+    if (!isset($_SESSION['user']['name'])) {
+        $_SESSION['errors'] = 'Чтобы отправлять сообщения необходимо авторизоваться!';
+        return false;
+    }
+
+    if (empty($message)) {
+        $_SESSION['errors'] = 'Введите текст сообщения!';
+        return false;
+    }
+
+    $res = $pdo->prepare("INSERT INTO messages (name, message) VALUES (?,?)");
+    if ($res->execute([$_SESSION['user']['name'], $message])) {
+        $_SESSION['success'] = 'Сообщение добавлено.';
+        return true;
+    } else {
+        $_SESSION['errors'] = 'Непредвиденная ошибка :(';
+        return false;
+    }
+}
+
+function get_messages(): array
+{
+    global $pdo;
+    $res = $pdo->query("SELECT * FROM messages");
+    return $res->fetchAll();
+}
---
+++ my_db.sql
@@ -0,0 +1,101 @@
+-- ВАЖНО!!! Это дамп тестовой базы данных. Импортировать его в БД с именем my_db.
+--
+--
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Апр 08 2023 г., 17:40
+-- Версия сервера: 5.7.39-log
+-- Версия PHP: 8.0.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `my_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `message` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `messages`
+--
+
+INSERT INTO `messages` (`id`, `name`, `message`, `created_at`) VALUES
+(1, 'Test', 'Тестовое сообщение №1', '2023-04-08 14:39:56');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `login` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pass` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `pass`) VALUES
+(1, 'Test', '$2y$10$1ESmUFpXTThYiZISNQTkE.99MC5XNNZwWzQZxi4bvRZ0IBA188Fk6');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
